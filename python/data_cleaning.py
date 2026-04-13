c6c2ccbc6b4881541c3b77cc729bfb695289e86d
Bal Krishan Sharma <48432142+balkrishan099@users.noreply.github.com>
2026-04-13 14:27:02 +0530

    Update data_cleaning.py

--- python/data_cleaning.py
+++ python/data_cleaning.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+df = pd.read_csv('data/client_sales_data.csv')
+
+df['order_dt'] = pd.to_datetime(df['order_dt'])
+
+df['profit_margin_pct'] = (df['profit_amount'] / df['revenue']) * 100
+
+print(df.head())
