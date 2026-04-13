03bfd69c1cb88c628acf2e05a2f0b88fc5c97018
Bal Krishan Sharma <48432142+balkrishan099@users.noreply.github.com>
2026-04-13 15:44:57 +0530

    Update sales_analysis.py

--- python/sales_analysis.py
+++ python/sales_analysis.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 df = pd.read_csv('data/cleaned_sales_data.csv')
 
+# ensure date format
+df['order_dt'] = pd.to_datetime(df['order_dt'])
+
 # monthly revenue
 monthly_sales = df.groupby(df['order_dt'].dt.to_period('M'))['revenue'].sum()
 
@@ -11,11 +14,11 @@ category_sales = df.groupby('product_category')['revenue'].sum()
 # profit by region
 region_profit = df.groupby('region')['profit_amount'].sum()
 
-print("Monthly revenue")
-print(monthly_sales.head())
+print("\nMonthly revenue")
+print(monthly_sales)
 
-print("Category sales")
-print(category_sales.head())
+print("\nRevenue by category")
+print(category_sales)
 
-print("Region profit")
-print(region_profit.head())
+print("\nProfit by region")
+print(region_profit)
