d9acc70431429e2a7f133b0c9df66b9d4392eab5
Bal Krishan Sharma <48432142+balkrishan099@users.noreply.github.com>
2026-04-13 15:02:53 +0530

    Update data_cleaning.py

--- python/data_cleaning.py
+++ python/data_cleaning.py
@@ -1,9 +1,19 @@
 import pandas as pd
 
+# load dataset
 df = pd.read_csv('data/client_sales_data.csv')
 
+# convert date column
 df['order_dt'] = pd.to_datetime(df['order_dt'])
 
+# remove duplicate rows
+df = df.drop_duplicates()
+
+# create new metric
 df['profit_margin_pct'] = (df['profit_amount'] / df['revenue']) * 100
 
+# save cleaned data
+df.to_csv('data/cleaned_sales_data.csv', index=False)
+
+print("data cleaned successfully")
 print(df.head())
